4f697a64395a9cfeb2cda13e4174a4e5fca6b2f9
matiellojr <matiello.junior@gmail.com>
2022-10-28 02:14:04 -0300

    #TCC
    Implementação de Validação do Campo CPF e do ID da tabela do associado por que quando insere o novo associado acontece o erro de chave primária, e não estava pegando o ultimo ID para somar.

--- gestao_associado/app.py
+++ gestao_associado/app.py
@@ -1,9 +1,12 @@
+from pickle import FALSE
+from webbrowser import get
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 import sqlalchemy as sa
 import enum
 from datetime import date
+from functions import validacao_cpf
 import psycopg2
 
 
@@ -35,7 +38,8 @@ class associados(db.Model):
     status_associado = sa.Column(sa.Boolean)
 
 
-    def __init__(self, cpf, nome_completo, endereco, data_cadastro, cidade, uf,  email, tipo_sanguineo, data_nascimento, data_atualizada, telefone, estado_civil, situacao_trabalho, como_identifica, quantidades_filhos, tem_filhos, status_associado):
+    def __init__(self, id, cpf, nome_completo, endereco, data_cadastro, cidade, uf,  email, tipo_sanguineo, data_nascimento, data_atualizada, telefone, estado_civil, situacao_trabalho, como_identifica, quantidades_filhos, status_associado):
+        self.id = id
         self.data_cadastro = data_cadastro
         self.data_atualizada = data_atualizada
         self.cpf = cpf
@@ -51,7 +55,6 @@ class associados(db.Model):
         self.situacao_trabalho = situacao_trabalho
         self.como_identifica = como_identifica
         self.quantidades_filhos = quantidades_filhos
-        self.tem_filhos = tem_filhos
         self.status_associado = status_associado
 
 
@@ -158,6 +161,7 @@ def page_associado_form():
     query_tipos_sanguineos = db.engine.execute("SELECT TIPOS FROM TIPOS_SANGUINEO;")
     query_identificacao = db.engine.execute('SELECT TIPO FROM IDENTIFICACAO;')
     query_estado_civil = db.engine.execute('SELECT TIPO FROM ESTADO_CIVIL;')
+    query_quantidade_associados = db.engine.execute(f"SELECT COUNT(*) FROM ASSOCIADOS;")
 
     data_atualizada = request.form.get('data_atualizada')
     data_cadastro = request.form.get('data_cadastro')
@@ -174,15 +178,20 @@ def page_associado_form():
     situacao_trabalho = request.form.get('situacao_trabalho')
     como_identifica = request.form.get('como_identifica')
     quantidades_filhos = request.form.get('quantidades_filhos')
-    status_associado = request.form.get('status_associado')
 
     if (request.method == 'POST'):
-            associado = associados(cpf, nome_completo, endereco, data_cadastro, cidade, uf, email, tipo_sanguineo, data_nascimento, data_atualizada, telefone, estado_civil, situacao_trabalho, como_identifica, quantidades_filhos, status_associado)
+        if not(validacao_cpf.Cpf.validate(request.form.get('cpf'))):
+            cpf_error = request.form.get('cpf')
+            flash(f"Favor prencher no campo CPF, que está incorreto: {cpf_error}!", "error")
+        else:
+            get_id = query_quantidade_associados.fetchone()
+            get_last_id = get_id[0] + 1
+            print(get_last_id)
+            associado = associados(get_last_id, cpf, nome_completo, endereco, data_cadastro, cidade, uf, email, tipo_sanguineo, data_nascimento, data_atualizada, telefone, estado_civil, situacao_trabalho, como_identifica, quantidades_filhos, False)
             db.session.add(associado)
             db.session.commit()
             return redirect(url_for('page_associado_lista'))
-
-    return render_template("associado_form.html", tipos = query_tipos_sanguineos,  identificacao = query_identificacao, estado_civil = query_estado_civil)
+    return render_template("associado_form.html", tipos = query_tipos_sanguineos,  identificacao = query_identificacao, estado_civil = query_estado_civil, data_hoje=date.today())
 
 
 
@@ -191,36 +200,40 @@ def page_associado_atualiza(id):
     query_associados = db.engine.execute(f"SELECT * FROM ASSOCIADOS WHERE ID = {id} ORDER BY ID;")
 
     if (request.method == 'POST'):
-        data_atualizada = date.today()
-        data_cadastro = request.form['data_cadastro']
-        cpf = request.form['cpf']
-        nome_completo = request.form['nome_completo']
-        endereco = request.form['endereco']
-        email = request.form['email']
-        cidade = request.form['cidade']
-        uf = request.form['uf']
-        tipo_sanguineo = request.form['tipo_sanguineo']
-        data_nascimento = request.form['data_nascimento']
-        telefone = request.form['telefone']
-        estado_civil = request.form['estado_civil']
-        situacao_trabalho = request.form['situacao_trabalho']
-        como_identifica = request.form['como_identifica']
-        quantidades_filhos = request.form['quantidades_filhos']
-        # tem_filhos = request.form['switch_filhos']
-        # https://www.youtube.com/watch?v=_sgVt16Q4O4
-
-        if (request.form.get('status_associado')):
-            status_associado = True
+        if not(validacao_cpf.Cpf.validate(request.form['cpf'])):
+            cpf_error = request.form['cpf']
+            flash(f"O CPF está incorreto: {cpf_error}!", "error")
         else:
-            status_associado = False
+            data_atualizada = date.today()
+            data_cadastro = request.form['data_cadastro']
+            cpf = request.form['cpf']
+            nome_completo = request.form['nome_completo']
+            endereco = request.form['endereco']
+            email = request.form['email']
+            cidade = request.form['cidade']
+            uf = request.form['uf']
+            tipo_sanguineo = request.form['tipo_sanguineo']
+            data_nascimento = request.form['data_nascimento']
+            telefone = request.form['telefone']
+            estado_civil = request.form['estado_civil']
+            situacao_trabalho = request.form['situacao_trabalho']
+            como_identifica = request.form['como_identifica']
+            quantidades_filhos = request.form['quantidades_filhos']
+            # tem_filhos = request.form['switch_filhos']
+            # https://www.youtube.com/watch?v=_sgVt16Q4O4
 
-        db.engine.execute(
-            f"""
-                UPDATE ASSOCIADOS SET DATA_CADASTRO='{data_cadastro}', DATA_ATUALIZADA='{data_atualizada}', CPF='{cpf}', NOME_COMPLETO='{nome_completo}', ENDERECO='{endereco}', CIDADE='{cidade}', UF='{uf}', EMAIL='{email}', DATA_NASCIMENTO='{data_nascimento}', TELEFONE='{telefone}', ESTADO_CIVIL='{estado_civil}', como_identifica='{como_identifica}', SITUACAO_TRABALHO='{situacao_trabalho}', TIPO_SANGUINEO='{tipo_sanguineo}', QUANTIDADES_FILHOS={quantidades_filhos}, STATUS_ASSOCIADO={status_associado} WHERE ID={id};
-            """)
+            if (request.form.get('status_associado')):
+                status_associado = True
+            else:
+                status_associado = False
 
-        db.session.commit()
-        return redirect(url_for('page_associado_lista'))
+            db.engine.execute(
+                f"""
+                    UPDATE ASSOCIADOS SET DATA_CADASTRO='{data_cadastro}', DATA_ATUALIZADA='{data_atualizada}', CPF='{cpf}', NOME_COMPLETO='{nome_completo}', ENDERECO='{endereco}', CIDADE='{cidade}', UF='{uf}', EMAIL='{email}', DATA_NASCIMENTO='{data_nascimento}', TELEFONE='{telefone}', ESTADO_CIVIL='{estado_civil}', como_identifica='{como_identifica}', SITUACAO_TRABALHO='{situacao_trabalho}', TIPO_SANGUINEO='{tipo_sanguineo}', QUANTIDADES_FILHOS={quantidades_filhos}, STATUS_ASSOCIADO={status_associado} WHERE ID={id};
+                """)
+            db.session.commit()
+            return redirect(url_for('page_associado_lista'))
+
     return render_template("associado_atualiza.html", _query_associados = query_associados)
 
 
@@ -228,47 +241,47 @@ def page_associado_atualiza(id):
 # Financeiro
 # --------------------------------------------
 # https://lala-rustamli.medium.com/casting-populated-table-column-to-enum-in-flask-with-sqlalchemy-f44fe404d9ae
-class TipoMensalidade(enum.Enum):
-    MENSAL = 'Mensal'
-    ANUAL  = 'Anual'
+# class TipoMensalidade(enum.Enum):
+#     MENSAL = 'Mensal'
+#     ANUAL  = 'Anual'
 
-class StatusMensalidade(enum.Enum):
-    status1 = 'Ainda falta pagar!'
-    status2 = 'Pago!'
-    status3 = 'Atrasado!'
+# class StatusMensalidade(enum.Enum):
+#     status1 = 'Ainda falta pagar!'
+#     status2 = 'Pago!'
+#     status3 = 'Atrasado!'
 
-class mensalidade(db.Model):
-    id = sa.Column(sa.Integer, primary_key = True)
-    tipo_mensalidade = sa.Column(sa.Enum(TipoMensalidade))
-    data_pagamento = sa.Column(sa.Date)
-    data_vencimento = sa.Column(sa.Date)
-    valor_mensalidade = sa.Column(sa.Float)
-    pagamento_confirmado = sa.Column(sa.Boolean)
-    status_mensalidade = sa.Column(sa.Enum(StatusMensalidade))
-    id_pagamento = relationship("pagamento", back_populates="mensalidade")
+# class mensalidade(db.Model):
+#     id = sa.Column(sa.Integer, primary_key = True)
+#     tipo_mensalidade = sa.Column(sa.Enum(TipoMensalidade))
+#     data_pagamento = sa.Column(sa.Date)
+#     data_vencimento = sa.Column(sa.Date)
+#     valor_mensalidade = sa.Column(sa.Float)
+#     pagamento_confirmado = sa.Column(sa.Boolean)
+#     status_mensalidade = sa.Column(sa.Enum(StatusMensalidade))
+#     id_pagamento = relationship("pagamento", back_populates="mensalidade")
 
-    def __init__(self, tipo_mensalidade, data_pagamento, data_vencimento,valor_mensalidade, pagamento_confirmado, status_mensalidade, id_pagamento):
-        self.tipo_mensalidade = tipo_mensalidade
-        self.data_pagamento = data_pagamento
-        self.data_vencimento = data_vencimento
-        self.valor_mensalidade = valor_mensalidade
-        self.pagamento_confirmado = pagamento_confirmado
-        self.status_mensalidade = status_mensalidade
-        self.id_pagamento = id_pagamento
+#     def __init__(self, tipo_mensalidade, data_pagamento, data_vencimento,valor_mensalidade, pagamento_confirmado, status_mensalidade, id_pagamento):
+#         self.tipo_mensalidade = tipo_mensalidade
+#         self.data_pagamento = data_pagamento
+#         self.data_vencimento = data_vencimento
+#         self.valor_mensalidade = valor_mensalidade
+#         self.pagamento_confirmado = pagamento_confirmado
+#         self.status_mensalidade = status_mensalidade
+#         self.id_pagamento = id_pagamento
 
 
-class pagamento(db.Model):
-    id = sa.Column(sa.Integer, primary_key = True)
-    data_pagamento = sa.Column(sa.Date)
-    valor_pagamento = sa.Column(sa.Float)
-    comprovante_pagamento = sa.Column(sa.String)
-    mensalidade_id = sa.Column(sa.Integer, sa.ForeignKey("mensalidade.id"))
+# class pagamento(db.Model):
+#     id = sa.Column(sa.Integer, primary_key = True)
+#     data_pagamento = sa.Column(sa.Date)
+#     valor_pagamento = sa.Column(sa.Float)
+#     comprovante_pagamento = sa.Column(sa.String)
+#     mensalidade_id = sa.Column(sa.Integer, sa.ForeignKey("mensalidade.id"))
 
-    def __init__(self, data_pagamento, valor_pagamento, comprovante_pagamento, mensalidade_id):
-        self.data_pagamento = data_pagamento
-        self.valor_pagamento = valor_pagamento
-        self.comprovante_pagamento = comprovante_pagamento
-        self.mensalidade_id = mensalidade_id
+#     def __init__(self, data_pagamento, valor_pagamento, comprovante_pagamento, mensalidade_id):
+#         self.data_pagamento = data_pagamento
+#         self.valor_pagamento = valor_pagamento
+#         self.comprovante_pagamento = comprovante_pagamento
+#         self.mensalidade_id = mensalidade_id
 
 
 @app.route('/financeiro_form' , methods=["GET", "POST"])
